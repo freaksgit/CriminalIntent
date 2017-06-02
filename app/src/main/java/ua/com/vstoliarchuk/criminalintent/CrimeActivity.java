7594485213236579ef2591aa4df735d20f0517f4
Vasyl Stoliarchuk <qloverock@gmail.com>
2017-06-02 12:03:55 +0300

    Chapter 10 is done

--- app/src/main/java/ua/com/vstoliarchuk/criminalintent/CrimeActivity.java
+++ app/src/main/java/ua/com/vstoliarchuk/criminalintent/CrimeActivity.java
@@ -1,12 +1,14 @@
 package ua.com.vstoliarchuk.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CrimeActivity extends SingleFragmentActivity {
+import java.util.UUID;
 
+public class CrimeActivity extends SingleFragmentActivity {
+public static final String EXTRA_CRIME_ID = "ua.com.vstoliarchuk.criminalintent.crime_id";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +16,13 @@ public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+        return CrimeFragment.newInstance(crimeId);
+    }
+
+    public static Intent newIntent(Context packageContext, UUID crimeId){
+        Intent intent = new Intent(packageContext, CrimeActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
     }
 }
--- app/src/main/java/ua/com/vstoliarchuk/criminalintent/CrimeListFragment.java
+++ app/src/main/java/ua/com/vstoliarchuk/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package ua.com.vstoliarchuk.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -19,21 +20,32 @@ import java.util.List;
  */
 public class CrimeListFragment extends Fragment {
     private RecyclerView mCrimeRecyclerView;
-
+    private CrimeAdapter mAdapter;
+    private int mChangedItemPosition;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
         mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        updateUI();
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
     }
 
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
-        mCrimeRecyclerView.setAdapter(new CrimeAdapter(crimes));
+        if (null == mAdapter){
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+        }else{
+            mAdapter.notifyItemChanged(mChangedItemPosition);
+        }
     }
 
 
@@ -57,7 +69,10 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View v) {
-            Toast.makeText(getActivity(), mCrime.getTitle() + " Clicked!", Toast.LENGTH_SHORT).show();
+            mChangedItemPosition = getAdapterPosition();
+            Intent intent = CrimeActivity.newIntent(getContext(), mCrime.getId());
+            startActivity(intent);
+            //Toast.makeText(getActivity(), mCrime.getTitle() + " Clicked!", Toast.LENGTH_SHORT).show();
         }
     }
 
